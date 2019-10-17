47c0e120245d221181d5288073b3e9d476a74e72
Donal <donal@Donals-MacBook-Air.local>
2019-10-17 17:10:57 +0100

    add user and messages resources

---
+++ app/models/user.rb
@@ -0,0 +1,15 @@
+class User < ActiveRecord::Base
+  # before an email is committed parse it to lowercase
+  before_save { self.email = email.downcase }
+
+  validates :username, presence: true, length: { minimum: 3, maximum: 15 }
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
+  validates :email,    presence: true,
+                       length:      { maximum: 105 },
+                       uniqueness:  { case_sensitive: false },
+                       format:      { with: VALID_EMAIL_REGEX }
+  has_many :messages
+  # enable salted password for authentication. Also use the Bcrypt gem
+  has_secure_password
+end
